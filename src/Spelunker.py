1cd774903c6c3f7a4304f13f45cf1c7fd852a8e9
sheikhrachel <sheikhisaac@gmail.com>
2020-10-03 13:41:42 -0700

    added ability to parse out each line from .py files

--- src/Spelunker.py
+++ src/Spelunker.py
@@ -1,1 +1,27 @@
-print("hello, spelunker")
+import os
+import shutil
+import time
+from fnmatch import fnmatch
+
+from mock_generator import file_reader
+
+root = os.getcwd()
+pattern = "*.py"
+
+try:
+    os.mkdir('test')
+except Exception as e:
+    pass
+
+for path, subdirs, files in os.walk(root):
+    for file in files:
+        if fnmatch(file, pattern):
+            current_file = os.path.join(path, file)
+            print(current_file)
+            file_reader(current_file)
+
+# python3 ../Spelunker/src/Spelunker.py 
+print("current directory: ", root)
+
+time.sleep(2)
+shutil.rmtree('test')
---
+++ src/mock_generator.py
@@ -0,0 +1,3 @@
+def file_reader(file):
+	f = open(file, "r")
+	print(f.readlines())
